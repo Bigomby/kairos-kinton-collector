9fef7b4729f8fb47a234309f3665fae4c23ed56e
Diego Fernández Barrera <bigomby@gmail.com>
2016-06-21 03:03:18 +0200

    Add WIP code

---
+++ index.js
@@ -0,0 +1,28 @@
+const SockJS = require('sockjs-client');
+const webstomp = require('webstomp-client');
+
+const ws = new SockJS('http://' + '172.20.0.2' + ':15674/stomp');
+const client = webstomp.over(ws, {
+  debug: false,
+});
+
+const headers = {
+  login: 'guest',
+  passcode: 'guest',
+  host: '/',
+  queue: '/topic/test',
+};
+
+client.connect(headers,
+  // Connection success
+  () => {
+    console.log('Connected');
+    client.subscribe(headers.queue, (m) => {
+      console.log(m.body);
+    });
+  },
+  // Connection error
+  () => {
+    console.log('Error connecting');
+  }
+);
